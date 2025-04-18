e554122cc5b42c511c7ed22a803cfec470ac2a1b
boomlauren <boomfrontenddeveloper@gmail.com>
2025-04-18 15:01:02 +0100

    Initial commit

---
+++ port.js
@@ -0,0 +1,6 @@
+// var typed = new Typed("#change-about", {
+//     strings: ["Boom Lauren", "a Frontend Developer", "a Social Media Manager"],
+//     typespeed: 150,
+//     backspeed: 100,
+//     loop: true
+// });
